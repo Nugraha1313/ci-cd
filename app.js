8fd9272d34551a723045163b3f17842ea1c78a51
Nugraha <aulia.nugraha13@gmail.com>
2023-05-21 17:31:49 +0700

    implement sentry node

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const router = require('./routes');
+const morgan = require("morgan");
+const router = require("./routes");
+const Sentry = require("@sentry/node");
 
-const {
-    HTTP_PORT = 3102
-} = process.env;
+const { SENTRY_DSN, ENVIRONMENT } = process.env;
+
+Sentry.init({
+  environment: ENVIRONMENT,
+  dsn: SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    // Automatically instrument Node.js libraries and frameworks
+    ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// const { HTTP_PORT = 3102 } = process.env;
 
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
+// RequestHandler creates a separate execution context, so that all
+// transactions/spans/breadcrumbs are isolated across requests
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 app.use(router);
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
 
 // 500
 app.use((err, req, res, next) => {
-    console.log(err);
-    return res.status(500).json({
-        status: false,
-        message: err.message,
-        data: null
-    });
+  console.log(err);
+  return res.status(500).json({
+    status: false,
+    message: err.message,
+    data: null,
+  });
 });
 
 // app.listen(HTTP_PORT, () => console.log('running on port', HTTP_PORT));
 
-module.exports = app
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,16 @@ router.get('/', (req, res) => {
         data: null
     })
 })
+router.get("/error", (req, res) => {
+  const data = {
+    status: true,
+    message: "ga error lagi",
+    data: null,
+  };
+
+  return res.status(200).json(data);
+});
+
 router.post('/auth/register', user.register);
 router.post('/auth/login', user.login);
 router.get('/auth/whoami', middlewares.auth, user.whoami);
